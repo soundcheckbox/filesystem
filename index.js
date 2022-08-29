a7da905d38397317ec83cdea67f8f34d48cdcaa0
Zababurin Sergei <zababurins@vk.com>
2022-08-30 00:27:40 +0300

    update

--- index.js
+++ index.js
@@ -11,6 +11,5 @@
 
 //TODO подключение тестов
 import { test } from '/modules/tests/frontend/src/index.mjs'
-(async () => {
-    test('/tests/fileSystem/index.test.js', true).catch(e => {console.log('error devtool', e)})
-})()
+
+test('/tests/fileSystem/index.test.js', true).catch(e => {console.log('error devtool', e)})
