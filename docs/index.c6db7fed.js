78a1d29dd85d872a6389654c388ae26f052e7d9b
Zababurin Sergei <zababurins@vk.com>
2022-08-30 00:24:55 +0300

    update

--- docs/index.c6db7fed.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.c6db7fed.js.map
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//TODO подключение файловой системы
+// import service from '/service/WCNode/src/fs/index.mjs'
+// (async () => {
+//  const api = await service()
+//  const idbfs = await api.idbfs()
+//  idbfs.info()
+//     idbfs.set.file('test', {test:'test2'})
+//     idbfs.save()
+//     console.log('ddddd', await idbfs.get.file('test'))
+// })()
+
+//TODO подключение тестов
 import { test } from '/modules/tests/frontend/src/index.mjs'
 (async () => {
     test('/tests/fileSystem/index.test.js', true).catch(e => {console.log('error devtool', e)})
